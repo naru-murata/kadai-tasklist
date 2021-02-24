83d98163e92c4596f23b8ca07682cd65b840987a
naru.murata <ubuntu@ip-172-31-20-62.ap-northeast-1.compute.internal>
2021-02-24 03:54:07 +0000

    user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_005848) do
+ActiveRecord::Schema.define(version: 2021_02_24_032256) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "status"
+    t.bigint "Usertask_id"
+    t.index ["Usertask_id"], name: "index_tasks_on_Usertask_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
